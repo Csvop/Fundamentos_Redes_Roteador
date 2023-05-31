993158701ee749979f6dae4a5da3f456a15c3f27
Bruno Breyer Garcia <bruno.garcia99@edu.pucrs.br>
2023-05-30 21:20:27 -0300

    first commit

---
+++ roteador/src/roteador/IpWithPort.java
@@ -0,0 +1,11 @@
+package roteador;
+
+public class IpWithPort {
+    public String ip;
+    public String port;
+
+    public IpWithPort(String ip, String port) {
+        this.ip = ip;
+        this.port = port;
+    }
+}
