66d4fc6b4737fc7295d5e6bc3398647fb496118f
Bruno Breyer Garcia <bruno.garcia99@edu.pucrs.br>
2023-06-01 20:42:38 -0300

    WIP

--- roteador/src/roteador/MessageReceiver.java
+++ roteador/src/roteador/MessageReceiver.java
@@ -44,6 +44,8 @@ public class MessageReceiver implements Runnable{
             
             /* Transforma a mensagem em string */
             String tabela_string = new String( receivePacket.getData());
+
+            System.out.println(tabela_string);
             
             /* Obtem o IP de origem da mensagem */
             InetAddress IPAddress = receivePacket.getAddress();
--- roteador/src/roteador/TabelaRoteamento.java
+++ roteador/src/roteador/TabelaRoteamento.java
@@ -1,32 +1,72 @@
 package roteador;
 
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TabelaRoteamento {
-    /*Implemente uma estrutura de dados para manter a tabela de roteamento. 
-     * A tabela deve possuir: IP Destino, Métrica e IP de Saída.
-    */
-    
-    public TabelaRoteamento(){
-    
+    /*
+     * Implemente uma estrutura de dados para manter a tabela de roteamento.
+     * A tabela deve possuir: IPs Destino, Métrica e IP de Saída.
+     */
+    List<String> ips_destino;
+    List<Integer> metricas;
+    List<String> ips_saida;
+
+    public TabelaRoteamento() {
+        this.ips_destino = new ArrayList<String>();
+        this.metricas = new ArrayList<Integer>();
+        this.ips_saida = new ArrayList<String>();
     }
-    
-    
-    public void update_tabela(String tabela_s,  InetAddress IPAddress){
-        /* Atualize a tabela de rotamento a partir da string recebida. */
-        
-        System.out.println( IPAddress.getHostAddress() + ": " + tabela_s);
-    
+
+    /* Atualize a tabela de rotamento a partir da string recebida. */
+    // System.out.println( IPAddress.getHostAddress() + ": " + tabela_s);
+    public void update_tabela(String tabela_s, InetAddress IPAddress) {
+        // Limpa caracteres UTF8
+        tabela_s = tabela_s.replaceAll("[^\\x00-\\x7F]", "");
+
+        if(tabela_s.equals("!")) {
+            return;
+        }
+
+        String[] tabela = tabela_s.split("\\*");
+
+        for (String ipAndMetric : tabela) {
+            String[] ipAndMetricArray = ipAndMetric.split(";");
+            String ip = ipAndMetricArray[0];
+            Integer metric = Integer.parseInt(ipAndMetricArray[1]);
+
+            if (ips_destino.contains(ip)) {
+                int index = ips_destino.indexOf(ip);
+                if (metricas.get(index) <= metric) {
+                    ips_destino.set(index, ip);
+                    metricas.set(index, metric);
+                    ips_saida.set(index, IPAddress.getHostAddress());
+                }
+            } else {
+                ips_destino.add(ip);
+                metricas.add(metric);
+                ips_saida.add(IPAddress.getHostAddress());
+            }
+        }
     }
-    
-    public String get_tabela_string(){
-        String tabela_string = "!"; /* Tabela de roteamento vazia conforme especificado no protocolo */
-        
-        /* Converta a tabela de rotamento para string, conforme formato definido no protocolo . */
-        
+
+    public String get_tabela_string() {
+        String tabela_string = ""; /* Tabela de roteamento vazia conforme especificado no protocolo */
+
+        if (ips_destino.size() == 0) {
+            tabela_string = "!";
+        } else {
+            /*
+             * Converta a tabela de rotamento para string, conforme formato definido no
+             * protocolo .
+             */
+            for (int i = 0; i < ips_destino.size(); i++) {
+                tabela_string += "*" + ips_destino.get(i) + ";" + metricas.get(i);
+            }
+        }
+
         return tabela_string;
     }
-    
 
-    
 }
