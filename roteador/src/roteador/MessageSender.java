26e5b14cb861be66fb95560484f79165106dca85
Bruno Breyer Garcia <bruno.garcia99@edu.pucrs.br>
2023-06-13 21:25:52 -0300

    manda insta 10 sec

--- roteador/src/roteador/MessageSender.java
+++ roteador/src/roteador/MessageSender.java
@@ -10,21 +10,22 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class MessageSender implements Runnable{
-    TabelaRoteamento tabela; /*Tabela de roteamento */
+public class MessageSender implements Runnable {
+    TabelaRoteamento tabela; /* Tabela de roteamento */
     ArrayList<String> vizinhos; /* Lista de IPs dos roteadores vizinhos */
-    
-    public MessageSender(TabelaRoteamento t, ArrayList<String> v){
+    TabelaRoteamento tabela_antiga; /* Tabela de roteamento antes da última atualização */
+
+    public MessageSender(TabelaRoteamento t, ArrayList<String> v) {
         tabela = t;
         vizinhos = v;
     }
-    
+
     @Override
     public void run() {
         DatagramSocket clientSocket = null;
         byte[] sendData;
         InetAddress IPAddress = null;
-        
+
         /* Cria socket para envio de mensagem */
         try {
             clientSocket = new DatagramSocket();
@@ -32,17 +33,19 @@ public class MessageSender implements Runnable{
             Logger.getLogger(MessageSender.class.getName()).log(Level.SEVERE, null, ex);
             return;
         }
-        
-        while(true){
-            
-            /* Pega a tabela de roteamento no formato string, conforme especificado pelo protocolo. */
+
+        while (true) {
+            /*
+             * Pega a tabela de roteamento no formato string, conforme especificado pelo
+             * protocolo.
+             */
             String tabela_string = tabela.get_tabela_string();
-               
+
             /* Converte string para array de bytes para envio pelo socket. */
             sendData = tabela_string.getBytes();
-            
+
             /* Anuncia a tabela de roteamento para cada um dos vizinhos */
-            for (String ip : vizinhos){
+            for (String ip : vizinhos) {
                 /* Converte string com o IP do vizinho para formato InetAddress */
                 try {
                     IPAddress = InetAddress.getByName(ip);
@@ -50,10 +53,13 @@ public class MessageSender implements Runnable{
                     Logger.getLogger(MessageSender.class.getName()).log(Level.SEVERE, null, ex);
                     continue;
                 }
-                
-                /* Configura pacote para envio da menssagem para o roteador vizinho na porta 5000*/
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 5000);         
-                
+
+                /*
+                 * Configura pacote para envio da menssagem para o roteador vizinho na porta
+                 * 5000
+                 */
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 5000);
+
                 /* Realiza envio da mensagem. */
                 try {
                     clientSocket.send(sendPacket);
@@ -61,19 +67,24 @@ public class MessageSender implements Runnable{
                     Logger.getLogger(MessageSender.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-            
-            /* Espera 10 segundos antes de realizar o próximo envio. CONTUDO, caso
+
+            /*
+             * Espera 10 segundos antes de realizar o próximo envio. CONTUDO, caso
              * a tabela de roteamento sofra uma alteração, ela deve ser reenvida aos
              * vizinho imediatamente.
              */
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(MessageSender.class.getName()).log(Level.SEVERE, null, ex);
+            if (tabela_antiga.equals(tabela)) {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(MessageSender.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
 
+            // Atualiza a tabela antiga
+            tabela_antiga = tabela;
         }
-        
+
     }
-    
+
 }
